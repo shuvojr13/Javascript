28b5151284753457dab84525946730aa71b2a8dd
Ahad Hossain <60065898+shuvojr13@users.noreply.github.com>
2023-09-22 14:36:02 +0600

    Update name.js

--- name.js
+++ name.js
@@ -1,5 +1,5 @@
 console.log("Abdul Ahad");
-console.log("Sep 13,2023");
+console.log("Sep 22,2023");
 console.log("It's Time to changed to life, No more have time ... ");
 console.log("I read in Comilla University");
 // Update to date 
